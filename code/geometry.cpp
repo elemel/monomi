b37cc912977d7f3f1585e4484799e648397bfa24
Mikael Lind <elemel@elemel.se>
2010-11-16 01:16:02 +0100

    fleshed out geometry module

--- code/geometry.cpp
+++ code/geometry.cpp
@@ -1,6 +1,8 @@
 #include "geometry.hpp"
 
+#include <algorithm>
 #include <cmath>
+#include <limits>
 
 namespace monomi {
     Vector2::Vector2() :
@@ -8,10 +10,28 @@ namespace monomi {
         y(0.0f)
     { }
 
+    Vector2::Vector2(float f) :
+        x(f),
+        y(f)
+    { }
+
     Vector2::Vector2(float x, float y) :
         x(x),
         y(y)
     { }
+
+    Vector2::Vector2(const Point2 &p) :
+        x(p.x),
+        y(p.y)
+    { }
+
+    float Vector2::length() const {
+        return std::sqrt(squaredLength());
+    }
+
+    float Vector2::squaredLength() const {
+        return dot(*this, *this);
+    }
 
     Vector2 Vector2::operator-() const
     {
@@ -44,14 +64,6 @@ namespace monomi {
         x /= f;
         y /= f;
         return *this;
-    }
-
-    float Vector2::length() const {
-        return std::sqrt(squaredLength());
-    }
-
-    float Vector2::squaredLength() const {
-        return dot(*this, *this);
     }
 
     float Vector2::normalize() {
@@ -117,6 +129,11 @@ namespace monomi {
     Point2::Point2(float x, float y) :
         x(x),
         y(y)
+    { }
+
+    Point2::Point2(const Vector2 &v) :
+        x(v.x),
+        y(v.y)
     { }
 
     Point2 &Point2::operator+=(const Vector2 &v)
@@ -189,6 +206,69 @@ namespace monomi {
         p2(p2)
     { }
 
+    Point2 Box2::center() const
+    {
+        return Point2(0.5f * (p1.x + p2.x), 0.5f * (p1.y + p2.y));
+    }
+
+    bool Box2::empty() const
+    {
+        return width() <= 0.0f || height() <= 0.0f;
+    }
+
+    float Box2::width() const
+    {
+        return p2.x - p1.x;
+    }
+
+    float Box2::height() const
+    {
+        return p2.y - p1.y;
+    }
+
+    float Box2::perimeter() const
+    {
+        return 2.0f * (width() + height());
+    }
+
+    float Box2::area() const
+    {
+        return width() * height();
+    }
+
+    void Box2::clear()
+    {
+        p1.x = std::numeric_limits<float>::infinity();
+        p1.y = std::numeric_limits<float>::infinity();
+        p2.x = -std::numeric_limits<float>::infinity();
+        p2.y = -std::numeric_limits<float>::infinity();
+    }
+
+    void Box2::grow(const Point2 &p)
+    {
+        p1.x = std::min(p1.x, p.x);
+        p1.y = std::min(p1.y, p.y);
+        p2.x = std::max(p2.x, p.x);
+        p2.y = std::max(p2.y, p.y);
+    }
+
+    void Box2::grow(const Box2 &b)
+    {
+        p1.x = std::min(p1.x, b.p1.x);
+        p1.y = std::min(p1.y, b.p1.y);
+        p2.x = std::max(p2.x, b.p2.x);
+        p2.y = std::max(p2.y, b.p2.y);
+    }
+
+    Box2 intersection(const Box2 &b1, const Box2 &b2)
+    {
+        float x1 = std::max(b1.p1.x, b2.p1.x);
+        float y1 = std::max(b1.p1.y, b2.p1.y);
+        float x2 = std::min(b1.p2.x, b2.p2.x);
+        float y2 = std::min(b1.p2.y, b2.p2.y);
+        return Box2(Point2(x1, y1), Point2(x2, y2));
+    }
+
     Circle::Circle() :
         radius(0.0f)
     { }
@@ -197,4 +277,101 @@ namespace monomi {
         center(center),
         radius(radius)
     { }
+
+    bool Circle::contains(const Point2 &p) const
+    {
+        return squaredDistance(center, p) <= radius * radius;
+    }
+
+    Box2 boundingBox(const LineSegment2 &s)
+    {
+        float x1 = std::min(s.p1.x, s.p2.x);
+        float y1 = std::min(s.p1.y, s.p2.y);
+        float x2 = std::max(s.p1.x, s.p2.x);
+        float y2 = std::max(s.p1.y, s.p2.y);
+        return Box2(Point2(x1, y1), Point2(x2, y2));
+    }
+
+    Box2 boundingBox(const Box2 &b)
+    {
+        return b;
+    }
+
+    Box2 boundingBox(const Circle &c)
+    {
+        return Box2(c.center - Vector2(c.radius),
+                    c.center + Vector2(c.radius));
+    }
+
+    Box2 boundingBox(const Box2 &b1, const Box2 &b2)
+    {
+        float x1 = std::min(b1.p1.x, b2.p1.x);
+        float y1 = std::min(b1.p1.y, b2.p1.y);
+        float x2 = std::max(b1.p2.x, b2.p2.x);
+        float y2 = std::max(b1.p2.y, b2.p2.y);
+        return Box2(Point2(x1, y1), Point2(x2, y2));
+    }
+
+    bool intersects(const Box2 &b1, const Box2 &b2)
+    {
+        return b1.p1.x <= b2.p2.x && b2.p1.x <= b1.p2.x &&
+               b1.p1.y <= b2.p2.y && b2.p1.y <= b1.p2.y;
+    }
+
+    bool intersects(const Circle &c1, const Circle &c2)
+    {
+        return squaredDistance(c1.center, c2.center) <=
+               (c1.radius + c2.radius) * (c1.radius + c2.radius);
+    }
+
+    bool intersects(const Box2 &b, const Circle &c)
+    {
+        if (b.p1.x <= c.center.x && c.center.x <= b.p2.x) {
+            return b.p1.y <= c.center.y + c.radius &&
+                   c.center.y - c.radius <= b.p2.y;
+        } else if (b.p1.y <= c.center.y && c.center.y <= b.p2.y) {
+            return b.p1.x <= c.center.x + c.radius &&
+                   c.center.x - c.radius <= b.p2.x;
+        } else {
+            return c.contains(b.p1) || c.contains(Point2(b.p2.x, b.p1.y)) ||
+                   c.contains(b.p2) || c.contains(Point2(b.p1.x, b.p2.y));
+        }
+    }
+
+    bool intersects(const Circle &c, const Box2 &b)
+    {
+        return intersects(b, c);
+    }
+
+    LineSegment2 separate(const Box2 &b1, const Box2 &b2)
+    {
+        Box2 i = intersection(b1, b2);
+        if (i.width() <= i.height()) {
+            float y = i.center().y;
+            if (b2.p2.x - b1.p1.x <= b1.p2.x - b2.p1.x) {
+                return LineSegment2(Point2(b1.p1.x, y), Point2(b2.p2.x, y));
+            } else {
+                return LineSegment2(Point2(b1.p2.x, y), Point2(b2.p1.x, y));
+            }
+        } else {
+            float x = i.center().x;
+            if (b2.p2.y - b1.p1.y <= b1.p2.x - b2.p1.x) {
+                return LineSegment2(Point2(x, b1.p1.y), Point2(x, b2.p2.y));
+            } else {
+                return LineSegment2(Point2(x, b1.p2.y), Point2(x, b2.p1.y));
+            }
+        }
+    }
+
+    LineSegment2 separate(const Circle &c1, const Circle &c2)
+    {
+        Vector2 normal = c2.center - c1.center;
+        if (normal.x == 0.0f && normal.y == 0.0f) {
+            normal.x = 1.0f;
+        } else {
+            normal.normalize();
+        }
+        return LineSegment2(c1.center + c1.radius * normal,
+                            c2.center - c2.radius * normal);
+    }
 }
--- code/geometry.hpp
+++ code/geometry.hpp
@@ -1,15 +1,24 @@
 #ifndef MONOMI_GEOMETRY_HPP
 #define MONOMI_GEOMETRY_HPP
 
+#include <boost/optional.hpp>
+
 namespace monomi {
     const float pi = 3.14159265358979323846264338327950288419716939937510f;
+
+    class Point2;
 
     class Vector2 {
     public:
         float x, y;
 
         Vector2();
+        explicit Vector2(float f);
         Vector2(float x, float y);
+        explicit Vector2(const Point2 &p);
+
+        float length() const;
+        float squaredLength() const;
 
         Vector2 operator-() const;
 
@@ -17,9 +26,6 @@ namespace monomi {
         Vector2 &operator-=(const Vector2 &v);
         Vector2 &operator*=(float f);
         Vector2 &operator/=(float f);
-
-        float length() const;
-        float squaredLength() const;
 
         float normalize();
     };
@@ -35,12 +41,15 @@ namespace monomi {
     Vector2 cross(const Vector2 &v);
     Vector2 normalize(const Vector2 &v);
 
+    class Box2;
+
     class Point2 {
     public:
         float x, y;
 
         Point2();
         Point2(float x, float y);
+        explicit Point2(const Vector2 &v);
 
         Point2 &operator+=(const Vector2 &v);
         Point2 &operator-=(const Vector2 &v);
@@ -71,7 +80,20 @@ namespace monomi {
 
         Box2();
         Box2(const Point2 &p1, const Point2 &p2);
+
+        Point2 center() const;
+        bool empty() const;
+        float width() const;
+        float height() const;
+        float perimeter() const;
+        float area() const;
+
+        void clear();
+        void grow(const Point2 &p);
+        void grow(const Box2 &b);
     };
+
+    Box2 intersection(const Box2 &b1, const Box2 &b2);
 
     class Circle {
     public:
@@ -80,7 +102,17 @@ namespace monomi {
 
         Circle();
         Circle(const Point2 &center, float radius);
+
+        bool contains(const Point2 &p) const;
     };
+
+    bool intersects(const Box2 &b1, const Box2 &b2);
+    bool intersects(const Circle &c1, const Circle &c2);
+    bool intersects(const Box2 &b, const Circle &c);
+    bool intersects(const Circle &c, const Box2 &b);
+
+    LineSegment2 separate(const Box2 &b1, const Box2 &b2);
+    LineSegment2 separate(const Circle &c1, const Circle &c2);
 }
 
 #endif // MONOMI_GEOMETRY_HPP
