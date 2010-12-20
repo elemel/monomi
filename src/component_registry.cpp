0aff8d6ee14113c08d26a755d3b23f225b64f746
Mikael Lind <elemel@elemel.se>
2010-12-20 21:13:26 +0100

    garbage collection for component registry

--- src/component_registry.cpp
+++ src/component_registry.cpp
@@ -2,19 +2,26 @@
 
 #include "component.hpp"
 
+#include <iostream>
+
 namespace monomi {
     void ComponentRegistry::add(boost::shared_ptr<Component> const &component)
     {
-        components_.push_back(component);
+        if (component) {
+            components_.push_back(component);
+        }
     }
 
     void ComponentRegistry::update(float dt)
     {
         typedef std::vector<boost::weak_ptr<Component> >::iterator Iterator;
-        for (Iterator i = components_.begin(); i != components_.end(); ++i) {
-            if (boost::shared_ptr<Component> component = i->lock()) {
+        Iterator i = components_.begin();
+        for (Iterator j = components_.begin(); j != components_.end(); ++j) {
+            if (boost::shared_ptr<Component> component = j->lock()) {
                 component->update(dt);
+                *i++ = *j;
             }
         }
+        components_.erase(i, components_.end());
     }
 }
