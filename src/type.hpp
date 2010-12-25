537dc80bb95ab2730ab803d9eb6265ca3247a1f2
Mikael Lind <elemel@elemel.se>
2010-12-25 16:36:03 +0100

    documented the Type class

--- src/type.hpp
+++ src/type.hpp
@@ -5,6 +5,8 @@
 #include <typeinfo>
 
 namespace monomi {
+    // Wrapper for std::type_info with value semantics and a richer comparison
+    // interface.
     class Type {
     public:
         explicit Type(std::type_info const &info = typeid(void));
@@ -23,6 +25,7 @@ namespace monomi {
     bool operator>=(Type const &left, Type const &right);
     bool operator>(Type const &left, Type const &right);
 
+    // Outputs the demangled, namespace-qualified type name.
     std::ostream &operator<<(std::ostream &out, Type const &type);
 }
 
