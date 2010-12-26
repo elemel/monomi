e7c484d53e97668eb06db8428ba47ff1e7a6c524
Mikael Lind <elemel@elemel.se>
2010-12-26 20:23:32 +0100

    documented the StateMachine class

--- src/state_machine.hpp
+++ src/state_machine.hpp
@@ -10,6 +10,9 @@
 namespace monomi {
     class State;
 
+    // State machine that always has a single current state.
+    //
+    // See: State
     class StateMachine :
         private boost::noncopyable
     {
@@ -17,14 +20,25 @@ namespace monomi {
         typedef boost::signal<void ()> TransitionSignal;
         typedef TransitionSignal::slot_type TransitionSlot;
 
+        // Construct a state machine with the specified state, which must not
+        // be null.
         explicit StateMachine(boost::shared_ptr<State> const &state);
 
+        // Return the current state.
         boost::shared_ptr<State> state();
+
+        // Return the current state.
         boost::shared_ptr<State const> state() const;
+
+        // Set the state. The specified state must not be null.
         void state(boost::shared_ptr<State> const &state);
 
+        // Update the state machine. During the update, the current state may
+        // transition to another. Also updates the current state.
         void update(float dt);
 
+        // Connect a transition slot. The slot will be invoked during each
+        // state transition.
         boost::signals::connection
         connectTransitionSlot(TransitionSlot const &slot);
 
