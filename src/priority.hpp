2db5afd9d97fe00ff0e7e260dfb5c68c5a6dd55f
Mikael Lind <elemel@elemel.se>
2010-12-28 14:30:20 +0100

    documented the Priority enum

--- src/priority.hpp
+++ src/priority.hpp
@@ -4,6 +4,8 @@
 namespace monomi {
     // Priority for actors. Actors with lower priority will be updated before
     // actors with higher priority.
+    //
+    // Set priority to <priorityCount> to skip all updates for the actor.
     enum Priority {
         scriptPriority,
         kinematicPriority,
